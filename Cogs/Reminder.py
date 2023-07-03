74fdc6e1dbb6ac5de6310f24bb89aa1f577882e4
BrandonAWong <brandon.alex.wong@gmail.com>
2023-07-02 19:20:00 -0700

    added new functions and changed Reminder cog to work with db

--- Cogs/Reminder.py
+++ Cogs/Reminder.py
@@ -2,41 +2,36 @@ import discord
 from discord.ext import commands, tasks
 from time import strftime
 import datetime
-import asyncio
 import random
 from util import *
 
 
 class Reminder(commands.Cog):
-    def __init__(self, bot: commands.Bot) -> None:
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
         with open('./text/facts.txt', 'r') as file:
             self.facts = file.read().splitlines()
         with open('./text/quotes.txt', 'r') as file:
             self.quotes = file.read().splitlines()
+        self.start_daily_reminder.start()
 
-    async def check_daily_reminder(self) -> None:
-        while True: 
-            if str(strftime('%H:%M')) == '16:30':
-                self.daily_reminder.start('hello')
-                break
-            await asyncio.sleep(60)
+    @tasks.loop(minutes=1)
+    async def start_daily_reminder(self) -> None:
+        if str(strftime('%H:%M')) == '08:30' or True:
+            self.daily_reminder.start()
+            self.start_daily_reminder.cancel()
 
-    @tasks.loop(hours=24)
-    async def daily_reminder(self, ctx: commands.Context) -> None:
-        for i in range (1, int(len(IDS)/2+1)):
-            channel_id = IDS['CHANNEL_ID_'+str(i)]
-            channel = self.bot.get_channel(channel_id) 
+    @tasks.loop(seconds=1)
+    async def daily_reminder(self, ctx: commands.Context=None) -> None:
+        server_ids: list[tuple] = return_server_ids()
+        channel_ids: list[tuple] = return_channel_ids()
+        for i in range(len(server_ids)):
             assignment_count_today = assignment_count_tomorrow = 0
             inner_value_today = inner_value_tomorrow = ''
-            course = canvas.get_course(IDS['COURSE_ID_'+str(i)])
-            assignments = course.get_assignments(
-                bucket = 'upcoming', 
-                order_by = 'due_at')
-            
+            assignments = return_assignments(server_ids[i][0])
             for assignment in assignments:
                 due_date = utc_to_pst(assignment.due_at_date, 'no_include_hour')
-                due_hour = str(utc_to_pst(assignment.due_at_date, 'only_hour'))
+                due_hour = return_due_date(assignment)
                 if str(datetime.date.today()) == due_date:
                     assignment_count_today += 1 
                     inner_value_today += f'\n{assignment_count_today}. {str(assignment)}\n~  Due at {due_hour}\n'
@@ -49,19 +44,27 @@ class Reminder(commands.Cog):
             if assignment_count_today == 0:
                 inner_value_today = 'Nothing due today 🥳'
             if assignment_count_tomorrow == 0:
-                inner_value_tomorrow = 'Nothing due tomorrow 🎉'
+                inner_value_tomorrow = 'Nothing due tomorrow 🤩'
 
             embed = discord.Embed(
                 title = f'⏰ {strftime("%A %m-%d")}',
                 color = 0xFFFF00)
-            embed.add_field(name = 'Assignments Due Today', value = inner_value_today, inline = False)
-            embed.add_field(name = 'Assignments Due Tomorrow', value = inner_value_tomorrow, inline = False)
+            embed.add_field(name = return_course(server_ids[i][0]),
+                            value = '\n')
+            embed.add_field(name = 'Assignments Due Today',
+                            value = inner_value_today, 
+                            inline = False)
+            embed.add_field(name = 'Assignments Due Tomorrow', 
+                            value = inner_value_tomorrow, 
+                            inline = False)
             
             if random.randint(0,1) == 1:
                 embed.set_footer(text = random.choice(self.facts))
             else:
-                embed.set_footer(text = random.choice(self.facts))
+                embed.set_footer(text = random.choice(self.quotes))
+            
+            channel = self.bot.get_channel(channel_ids[i][0])
             await channel.send(embed=embed)
 
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(Reminder(bot))
+    await bot.add_cog(Reminder(bot)) 
--- util.py
+++ util.py
@@ -15,11 +15,11 @@ def upload_row(server_id: int, channel_id: int, org: str, course_id: int, token:
                 (server_id, channel_id, org, course_id, token))
     con.commit()
     con.close()
-    return validate_upload(server_id, course_id)
+    return validate_upload(server_id)
     
-def validate_upload(server_id: int, course_id: int) -> bool:
+def validate_upload(server_id: int) -> bool:
     try:
-        create_canvas(server_id).get_course(course_id)
+        return_course(server_id)
         return True
     except:
         return False
@@ -30,6 +30,20 @@ def delete_row(server_id):
     con.commit()
     con.close()
 
+def return_server_ids() -> list[tuple]:
+    con, cur = create_connection()
+    res = cur.execute("SELECT server_id FROM server")
+    info = res.fetchall()
+    con.close()
+    return info
+
+def return_channel_ids() -> list[tuple]:
+    con, cur = create_connection()
+    res = cur.execute("SELECT channel_id FROM server")
+    info = res.fetchall()
+    con.close()
+    return info
+
 def create_canvas(server_id: int) -> Canvas:
     con, cur = create_connection()
     res = cur.execute(f"SELECT org, token FROM server WHERE server_id = {server_id}")
@@ -38,16 +52,20 @@ def create_canvas(server_id: int) -> Canvas:
     con.close()
     return Canvas(url, key)
 
-def return_assignments(server_id: int) -> list:
-    canvas = create_canvas(server_id)
+def return_course(server_id: int) -> str:
     con, cur = create_connection()
+    canvas = create_canvas(server_id)
     res = cur.execute(f"SELECT course_id FROM server WHERE server_id = {server_id}")
     course_id: int = res.fetchone()[0]
     course = canvas.get_course(course_id)
-    assignments = course.get_assignments(
-        bucket = 'future', 
-        order_by = 'due_at')
     con.close()
+    return course
+
+def return_assignments(server_id: int) -> list:
+    course = return_course(server_id)
+    assignments = course.get_assignments(
+        bucket = 'upcoming', 
+        order_by = 'due_at')
     return assignments
 
 def return_assignments_url(server_id: int) -> str:
